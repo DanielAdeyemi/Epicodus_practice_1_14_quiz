0f63b38a3fbe6247a0834b366e92ea384291f014
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-14 19:48:49 -0800

    finish functions to evaluate user's input and get output accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,29 @@ $(document).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
     let name = $("#name").val();
-    $("#nameOut").text(name);
-    $(".output").show();
-    $("form").hide();
+    let total = 0;
+    let g = 0;
+    let choice = 0;
+    if ($("#gender").val() === "Male") {
+      g = 1;
+    } else if ($("#gender").val() === "Female") {
+      g = 10;
+    }
+    if ($("input:radio[name=celebrity]:checked").val() === "char") {
+      choice = 100;
+    } else if ($("input:radio[name=celebrity]:checked").val() === "cel") {
+      choice = 1;
+    }
+    total = g + choice;
+    if (total < 10) {
+
+    } else if (total < 100) {
+
+    } else if (total > 100) {
+      $("#nameOut").text(name);
+      $(".output3").show();
+      $("form").hide();
+
+    }
   });
 });
