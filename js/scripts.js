b83949cbb195f5c3fe621b79041139c963fc32fd
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-14 16:33:58 -0800

    working on displaying name

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
+    let name = $("#name").val();
+    $("#nameOut").text(name);
     $(".output").show();
     $("form").hide();
-  })
-})
+  });
+});
